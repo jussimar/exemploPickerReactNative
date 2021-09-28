4b5be34d81144e8f2b4d2ec1d96c630e33bf44a7
jussimar <jussimar@jussimarleal.com.br>
2021-09-27 21:05:35 -0300

    codigo de exemplo

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pizza: 0,
+      pizzas:[
+        {key:1, nome:'Calabresa', valor:39.90},
+        {key:2, nome:'Calabresa com Queijo', valor:59.90},
+        {key:3, nome:'Brigadeiro', valor:49.90}
+      ]
+    }
+  }
+  render(){
+    let pizzasItem = this.state.pizzas.map((v,k)=>{
+      return  <Picker.Item key={k} label={v.nome} value={k} />
+    })
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>teste</Text>
+        <Picker  selectedValue={this.state.pizza}
+            onValueChange={
+              (itemValue, itemIndex) => this.setState({pizza: itemValue})
+            }
+         >
+           {pizzasItem}
+          
+        </Picker>
+        <Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
+        <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
+      </View>
+    );
+  }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:20
   },
+  logo:{
+    textAlign:'center',
+    fontSize:28,
+    fontWeight:'bold'
+  },
+  pizzas:{
+    marginTop:15,
+    fontSize:28,
+    textAlign:'center'
+  }
 });
